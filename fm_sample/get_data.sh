393b8903c806fbe619eb42e040aca60dd0be0761
wararaki <>
2018-10-29 01:32:08 +0900

    fix shell

--- fm_sample/get_data.sh
+++ fm_sample/get_data.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # download data.
-# wget http://s3-eu-west-1.amazonaws.com/yc-rdata/yoochoose-dataFull.7z
-
 wget http://files.grouplens.org/datasets/movielens/ml-latest-small.zip
 
 # brew install p7zip
-# 7z x yoochoose-dataFull.7z
 unzip ml-latest-small.zip
 
 echo "Done"
