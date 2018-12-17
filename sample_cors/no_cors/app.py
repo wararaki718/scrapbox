f151a673747fd63f225049be9916568f47bc008e
wararaki <>
2018-12-18 01:27:00 +0900

    update

--- sample_cors/no_cors/app.py
+++ sample_cors/no_cors/app.py
@@ -1,12 +1,10 @@
 from flask import Flask
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route("/msg")
 def search():
-    return "hello"
+    return "cors prohibit"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5555, debug=False)
+    app.run(host='0.0.0.0', port=5556, debug=False)
