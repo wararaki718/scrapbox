8dcb5116a895876aaf914c64d776ffd90cb5297c
kyohei.watarai <kyohei.watarai@bizreach.co.jp>
2019-01-23 19:48:34 +0900

    update

---
+++ py_pkg_sample/pkg_with_item/hello/hello.py
@@ -0,0 +1,17 @@
+import os
+from site import getsitepackages
+
+import papermill as pm
+
+
+def hello_call(parameters):
+    base_notebook_path = os.path.join(os.path.dirname(__file__), 'base.ipynb')
+
+    nb = pm.execute_notebook(
+        base_notebook_path,
+        'out.ipynb',
+        parameters=parameters,
+        log_output=True
+    )
+    return nb
+
