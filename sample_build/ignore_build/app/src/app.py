c2f34f312f5a8dc6ec1d1689cb3183ba9c4faa28
kyohei.watarai <kyohei.watarai@bizreach.co.jp>
2019-03-19 11:15:34 +0900

    update

---
+++ sample_build/ignore_build/app/src/app.py
@@ -0,0 +1,12 @@
+import sys
+
+from base import hello
+
+
+def main():
+    print(hello())
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ sample_build/ignore_build/app/src/base.py
@@ -0,0 +1,7 @@
+
+def hello():
+    return 'hello'
+
+
+if __name__ == '__main__':
+    print(hello())
