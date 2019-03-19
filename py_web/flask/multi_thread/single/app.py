61dc00c512a53dea9d3faeaecd2d164b38a0a43e
wararaki <>
2019-03-20 02:21:04 +0900

    update

---
+++ py_web/flask/multi_thread/single/app.py
@@ -0,0 +1,16 @@
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    start = time.time()
+    time.sleep(5)
+    finish = time.time() - start
+    return f'api: {finish}'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5555, debug=False)
