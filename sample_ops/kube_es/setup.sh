b2a29e2382cbb5803724ba7faa4e14f61f349523
kyohei.watarai <kyohei.watarai@bizreach.co.jp>
2019-04-24 20:02:07 +0900

    working

---
+++ sample_ops/kube_es/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# download data
+### soseki natsume
+svn export https://github.com/aozorabunko/aozorabunko/trunk/cards/000148
+
+### dazai
+svn export https://github.com/aozorabunko/aozorabunko/trunk/cards/000035
+
