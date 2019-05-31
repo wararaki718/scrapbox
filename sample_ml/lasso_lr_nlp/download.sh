6861254ba86cfcefd6222ca50875142995bf2aeb
wararaki <>
2019-06-01 01:45:53 +0900

    update

---
+++ sample_ml/lasso_lr_nlp/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# download data
+### soseki natsume
+svn export https://github.com/aozorabunko/aozorabunko/trunk/cards/000148 data/natsume
+
+### dazai
+svn export https://github.com/aozorabunko/aozorabunko/trunk/cards/000035 data/dazai
+
